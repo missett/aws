0ee0290b42527b2b406b5bcd79ae6d8c83b7c59f
Ryan Missett <ryan.missett@skybettingandgaming.com>
2022-02-11 14:33:49 +0000

    Add lambda basic permissions for deploy credentials

--- iam/blog.tf
+++ iam/blog.tf
@@ -12,17 +12,17 @@ data "aws_iam_policy_document" "ryanmissett_blog_github_deploy_policy" {
       "s3:PutObject",
     ]
     resources = [
-      "arn:aws:s3:::ryanmissett-terraform-backend/blog"
+      "arn:aws:s3:::ryanmissett-terraform-backend/blog",
     ]
   }
 
   statement {
     effect = "Allow"
     actions = [
-      "s3:ListBucket"
+      "s3:ListBucket",
     ]
     resources = [
-      "arn:aws:s3:::ryanmissett-terraform-backend"
+      "arn:aws:s3:::ryanmissett-terraform-backend",
     ]
   }
 
@@ -31,7 +31,7 @@ data "aws_iam_policy_document" "ryanmissett_blog_github_deploy_policy" {
   statement {
     effect = "Allow"
     actions = [
-      "s3:*"
+      "s3:*",
     ]
     resources = [
       "arn:aws:s3:::ryanmissett-blog-frontend",
@@ -42,10 +42,20 @@ data "aws_iam_policy_document" "ryanmissett_blog_github_deploy_policy" {
   statement {
     effect = "Allow"
     actions = [
-      "cloudfront:*"
+      "cloudfront:*",
     ]
     resources = [
       "arn:aws:cloudfront::635451031443:*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:GetRole",
+    ]
+    resources = [
+      aws_iam_role.ryanmissett_blog_lambda_hello_world.arn,
     ]
   }
 }
@@ -54,4 +64,39 @@ resource "aws_iam_user_policy" "ryanmissett_blog_github_deploy_policy" {
   name = "ryanmissett_blog_github_deploy_policy"
   user = aws_iam_user.ryanmissett_blog_github_deploy.name
   policy = data.aws_iam_policy_document.ryanmissett_blog_github_deploy_policy.json
+}
+
+# lambda deploy functionality
+
+data "aws_iam_policy" "aws_lambda_full_access" {
+  name = "AWSLambda_FullAccess"
+}
+
+resource "aws_iam_user_policy" "ryanmissett_blog_github_deploy_policy_lambdas" {
+  name = "ryanmissett_blog_github_deploy_policy_lambdas"
+  user = aws_iam_user.ryanmissett_blog_github_deploy.name
+  policy = data.aws_iam_policy.aws_lambda_full_access.policy
+}
+
+# lambda execution role setup
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    effect = "Allow"
+  }
+}
+
+data "aws_iam_policy" "aws_lambda_basic_execution_role" {
+  name = "AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role" "ryanmissett_blog_lambda_hello_world" {
+  name = "ryanmissett_blog_lambda_hello_world"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json 
+  managed_policy_arns = [data.aws_iam_policy.aws_lambda_basic_execution_role.arn]
 }

86372fc8140868e20e147569bdae8c1ecc735d1a
Ryan Missett <ryan.missett@skybettingandgaming.com>
2022-02-18 08:23:54 +0000

    WIP Adding API gateway permissions

--- iam/blog.tf
+++ iam/blog.tf
@@ -100,3 +100,38 @@ resource "aws_iam_role" "ryanmissett_blog_lambda_hello_world" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json 
   managed_policy_arns = [data.aws_iam_policy.aws_lambda_basic_execution_role.arn]
 }
+
+# api gateway 
+
+data "aws_iam_policy" "aws_api_gateway_full_access" {
+  name = "AmazonAPIGatewayAdministrator"
+}
+
+resource "aws_iam_user_policy" "ryanmissett_blog_github_deploy_policy_api_gateway" {
+  name = "ryanmissett_blog_github_deploy_policy_api_gateway"
+  user = aws_iam_user.ryanmissett_blog_github_deploy.name
+  policy = data.aws_iam_policy.aws_api_gateway_full_access.policy
+}
+
+data "aws_iam_policy_document" "ryanmissett_blog_api_gateway_invoke_lambda" {
+  statement {
+    effect = "Allow"
+    actions = ["lambda:InvokeFunction"]
+    resources = [
+      "arn:aws:lambda:eu-west-2:635451031443:function:ryanmissett_blog_hello_world",
+      "arn:aws:lambda:eu-west-2:635451031443:function:ryanmissett_blog_hello_again",
+    ]
+    condition {
+      test = "ArnLike"
+      variable = "aws:SourceArn"
+      values = [
+        "arn:aws:execute-api:eu-west-2:635451031443:pn0jiowmh1/*/*/"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "ryanmissett_blog_api_gateway_invoke_lambda" {
+  name = "ryanmissett_blog_api_gateway_invoke_lambda"
+  policy = data.aws_iam_policy_document.ryanmissett_blog_api_gateway_invoke_lambda.json
+}
